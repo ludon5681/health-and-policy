d7c0c79be96eb970ee89c867b3f26ca1842a1760
ludon5681 <lucadonais@gmail.com>
2022-10-12 12:10:55 -0400

    Added favicon.ico and works cited.html

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 document.addEventListener("DOMContentLoaded", function() {
 	OverlayScrollbars(document.querySelectorAll("body"), { });
 });
+
+$(".dropdown-item").on("click", (e) => {
+	e.preventDefault()
+	console.log(e.currentTarget.id)
+})
